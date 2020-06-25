169d2effba7e9ff2ad056309272f5311ca307c18
Jeffrey <Jwan622@gmail.com>
2020-06-25 10:09:04 -0400

    WIP: failing incomplete test also deleted some serverless comments

--- spec/quote_spec.rb
+++ spec/quote_spec.rb
@@ -1,13 +1,27 @@
 require '../quote_handler'
 describe 'quote_controller' do
-  it 'gets quotes' do
+  before do
+    allow(HTTParty).to receive(:get).and_return(tradier_response)
+    allow(JSON).to receive(:parse)
+  end
+
+  it 'gets security quotes' do
+    let(:trader_response) { instance_double(HTTParty::Response, body: tradier_response_body) }
+    let(:tradier_response_body) { {securities: { security: {symbol: "AAPL" } } } }
+
     event = { queryStringParameters: {symbol: 'AAPL'}}
     context = {}
 
     response = lambdaHandler(event: event, context: context)
-    expected = "blee bloo"
-
+    expected = TODO
     expect(response).to equal(expected)
   end
 
+  it 'fetches from the correct endpoint' do
+    expect(HTTParty).to have_received(:get).with(github_url)
+  end
+
+  it 'parses the tradier response' do
+    expect(JSON).to have_received(:parse).with(tradier_response_body)
+  end
 end
